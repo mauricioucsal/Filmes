46dbe948db3afdaa2f8889844293b4fb331c178c
Mauricio Gabriel <souzamauricio91@hotmail.com>
2025-05-25 22:13:19 -0300

    feat: adicionando gifs

--- script.js
+++ script.js
@@ -3,61 +3,61 @@ const filmes = [
     titulo: "Matrix (1999)",
     descricao:
       "Um hacker descobre a verdade sobre sua realidade e seu papel na guerra contra seus controladores.",
-    imagem: "imagens/Matrix.jpg",
+    imagem: "imagens/Matrix.gif",
   },
   {
     titulo: "Logan (2017)",
     descricao:
-      "Em um futuro próximo, um Logan envelhecido cuida do Professor X enquanto foge de um grupo de vilões.",
-    imagem: "imagens/Logan_.jpg",
+      "Em um futuro próximo, um Logan envelhecido cuida do Professor Charles Xavier enquanto ambos fogem de um grupo de vilões.",
+    imagem: "imagens/Logan.gif",
   },
   {
     titulo: "Batman: O Cavaleiro das Trevas (2008)",
     descricao:
       "Batman enfrenta o Coringa em uma luta por Gotham e pela alma de seus cidadãos.",
-    imagem: "imagens/batman.jpg",
+    imagem: "imagens/batman.gif",
   },
   {
     titulo: "O Exterminador do Futuro 2: O Julgamento Final (1991)",
     descricao:
-      "Um ciborgue é enviado para proteger John Connor do exterminador T-1000.",
-    imagem: "imagens/O_exterminador_do_futuro.jpeg",
+      "Um ciborgue que volta do futuro para proteger John Connor de um exterminador avançado, garantindo a sobrevivência da resistência humana.",
+    imagem: "imagens/o_exterminador_do_futuro.gif",
   },
   {
     titulo: "Pantera Negra (2018)",
     descricao:
-      "T'Challa retorna a Wakanda como rei, mas enfrenta inimigos internos e externos.",
-    imagem: "imagens/pantera_negra.jpg",
+      "T’Challa enfrenta Erik Killmonger, um guerreiro com laços com Wakanda que desafia o trono com ideais radicais sobre poder e justiça.",
+    imagem: "imagens/pantera_negra.gif",
   },
   {
     titulo: "Interestelar (2014)",
     descricao:
-      "Exploradores viajam por um buraco de minhoca em busca de um novo lar para a humanidade.",
-    imagem: "imagens/interstellar.jpg",
+      "Um grupo de astronautas, liderados por Cooper, que viaja por um buraco de minhoca em busca de um novo lar para a humanidade, enquanto a Terra enfrenta uma crise ambiental.",
+    imagem: "imagens/interstellar.gif",
   },
   {
     titulo: "Projeto X - Uma Festa Fora de Controle (2012)",
     descricao:
       "Três adolescentes organizam uma festa épica que sai totalmente do controle.",
-    imagem: "imagens/projeto_x.jpg",
+    imagem: "imagens/projeto_x.gif",
   },
   {
     titulo: "Vingadores: Ultimato (2019)",
     descricao:
       "Os Vingadores tentam desfazer os eventos catastróficos causados por Thanos.",
-    imagem: "imagens/ultimato.jpg",
+    imagem: "imagens/ultimato.gif",
   },
   {
     titulo: "Vingadores: Guerra Infinita (2018)",
     descricao:
       "Os heróis do universo Marvel enfrentam Thanos em uma batalha épica.",
-    imagem: "imagens/guerra_infinita.jpg",
+    imagem: "imagens/guerra_infinita.gif",
   },
   {
     titulo: "Anjos da Lei 2 (2014)",
     descricao:
       "Schmidt e Jenko se infiltram na faculdade para desvendar um novo caso.",
-    imagem: "imagens/anjos_da_lei.jpg",
+    imagem: "imagens/anjos_da_lei.gif",
   },
 ];
 
